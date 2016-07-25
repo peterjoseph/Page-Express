241296bf0316445c02133bb33789aae623d56df7
Peter Joseph <contact@peterjoseph.net>
2016-07-25 21:26:19 +0800

    Adjusted comments html layout

--- comments.php
+++ comments.php
@@ -1,44 +1,45 @@
 <div id="comments">
     <?php if ( post_password_required() ) : ?>
-    <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'objecttheme' ); ?></p>
-</div>
-<?php
-return;
-endif;
-?>
+        <p class="nopassword">
+            <?php _e( 'This post is password protected.', 'objecttheme' ); ?>
+        </p>
+    <?php return; endif; ?>
 
-<?php if ( have_comments() ) : ?>
-    <h3 id="comments-title"><?php
-        printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'objecttheme' ),
-            number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
-        ?></h3>
+    <?php if ( have_comments() ) : ?>
+        <h3 class="comments_title">
+            <?php
+                printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'objecttheme' ),
+                number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
+            ?>
+        </h3>
 
-    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-        <div class="navigation">
-            <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'objecttheme' ) ); ?></div>
-            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'objecttheme' ) ); ?></div>
-        </div> <!-- .navigation -->
-    <?php endif; // check for comment navigation ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+            <div class="comments_navigation">
+                <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'objecttheme' ) ); ?></div>
+                <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'objecttheme' ) ); ?></div>
+            </div>
+        <?php endif; ?>
 
-    <ol class="commentlist">
-        <?php wp_list_comments( $args, $comments ); ?>
-    </ol>
+        <ol class="comments_list">
+            <?php wp_list_comments( $args, $comments ); ?>
+        </ol>
 
-    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-        <div class="navigation">
-            <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'objecttheme' ) ); ?></div>
-            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'objecttheme' ) ); ?></div>
-        </div><!-- .navigation -->
-    <?php endif; // check for comment navigation ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+            <div class="comments_navigation">
+                <div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments', 'objecttheme' ) ); ?></div>
+                <div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>', 'objecttheme' ) ); ?></div>
+            </div>
+        <?php endif; ?>
 
-    <?php
+        <?php
 
-    if ( ! comments_open() && get_comments_number() ) : ?>
-        <p class="nocomments"><?php _e( 'Comments are closed.' , 'objecttheme' ); ?></p>
-    <?php endif;  ?>
+        if ( ! comments_open() && get_comments_number() ) : ?>
+            <p class="comments_empty">
+                <?php _e( 'Comments are closed.' , 'objecttheme' ); ?>
+            </p>
+        <?php endif;  ?>
 
-<?php endif; // end have_comments() ?>
+    <?php endif; ?>
 
-<?php comment_form(); ?>
-
+    <?php comment_form(); ?>
 </div>
